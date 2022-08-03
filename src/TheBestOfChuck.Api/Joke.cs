fe83ea9d19478bda189c0bda08962dd719302c1b
Kamil Jaworski <kamil.jaworski@banqsoft.pl>
2022-08-03 18:07:53 +0200

    Implemented pulling jokes

---
+++ src/TheBestOfChuck.Api/Joke.cs
@@ -0,0 +1,8 @@
+﻿namespace TheBestOfChuck.Api
+{
+    public class Joke
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
--- src/TheBestOfChuck.Api/PullJokes.cs
+++ src/TheBestOfChuck.Api/PullJokes.cs
@@ -1,16 +1,66 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace TheBestOfChuck.Api
 {
     public class PullJokes
     {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public PullJokes(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
         [FunctionName(nameof(PullJokes))]
-        public void Run([TimerTrigger("%PullJokesSchedule%")]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("%PullJokesSchedule%")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            try
+            {
+                var client = _clientFactory.CreateClient();
+                client.BaseAddress = new Uri("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/");
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "3f73dc77dbmshc8497504b4101dbp164fb1jsndd6506616cbf");
+
+                var numberOfJokesToFetch = 10;
+                var jokes = new List<Joke>();
+
+                while (numberOfJokesToFetch > 0)
+                {
+                    var response = await client.GetAsync("jokes/random");
+                    response.EnsureSuccessStatusCode();
+
+                    var responseAsString = await response.Content.ReadAsStringAsync();
+                    var joke = JsonSerializer.Deserialize<Joke>(responseAsString,
+                        new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        });
+
+                    log.LogInformation(responseAsString);
+
+                    if (joke.Value.Length > 200)
+                    {
+                        log.LogInformation("Joke skipped, because it was too long.");
+                        continue;
+                    }
+
+                    jokes.Add(joke);
+                    numberOfJokesToFetch--;
+                }
+            }
+            catch (Exception exception)
+            {
+                log.LogError(exception, $"Failed to run {nameof(PullJokes)} function.");
+
+                throw;
+            }
         }
     }
 }
